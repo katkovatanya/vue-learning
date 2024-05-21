e667b0c99363dff330a2d02f112656dd3c424681
Tanya Katkova <yndx-katkovatanya@yandex.ru>
2024-05-21 10:58:40 +0300

    декомпозиция, пропсы

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <post-form />
+    <post-form @create="createPost" />
     <post-list :posts="posts" />
   </div>
 </template>
@@ -24,20 +24,11 @@ export default {
         { id: 2, title: "JavaScript 2", body: "Описнаие поста 3" },
         { id: 3, title: "JavaScript 3", body: "Описнаие поста 3" },
       ],
-      title: "",
-      body: "",
     };
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      };
-      this.posts.push(newPost);
-      this.title = "";
-      this.body = "";
+    createPost(post) {
+      this.posts.push(post);
     },
   },
 };
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,24 +2,43 @@
   <form @submit.prevent>
     <h4>Создание поста</h4>
     <input
-      v-bind:value="title"
-      @input="title = $event.target.value"
+      v-model="post.title"
       class="input"
       type="text"
       placeholder="Название"
     />
     <input
-      v-bind:value="body"
-      @input="body = $event.target.value"
+      v-model="post.body"
       class="input"
       type="text"
       placeholder="Описание"
     />
-    <button class="btn">Создать</button>
+    <button class="btn" @click="createPost">Создать</button>
   </form>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
