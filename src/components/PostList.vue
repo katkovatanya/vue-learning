ed4fdfa945b757abc0781926dac2a86c7b837687
Tanya Katkova <yndx-katkovatanya@yandex.ru>
2024-05-21 17:15:07 +0300

    условный рендеринг

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
+  <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
-    <post-item v-for="post in posts" :post="post" />
+    <post-item
+      v-for="post in posts"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    />
   </div>
+  <h2 v-else style="color: red">Список постов пуст</h2>
 </template>
 
 <script>
@@ -18,6 +24,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
