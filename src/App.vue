3692df5882f5d5c159f4d79d76a190245632371b
Tanya Katkova <yndx-katkovatanya@yandex.ru>
2024-05-21 20:45:15 +0300

    api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-button @click="showDialog"
-    style="margin: 15px 0;"
-    >Создать пользователя</my-button>
+    <my-button @click="fetchPosts">Получить посты</my-button>
+    <input type="text" v-model="modificatorValue" />
+    <my-button @click="showDialog" style="margin: 15px 0"
+      >Создать пользователя</my-button
+    >
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
@@ -15,6 +17,8 @@
 import MyButton from "./components/UI/MyButton";
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
+import axios from "axios";
+
 export default {
   components: {
     MyButton,
@@ -23,28 +27,31 @@ export default {
   },
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          title: "JavaScript",
-          body: "Описание поста",
-        },
-        { id: 2, title: "JavaScript 2", body: "Описнаие поста 3" },
-        { id: 3, title: "JavaScript 3", body: "Описнаие поста 3" },
-      ],
+      posts: [],
       dialogVisible: false,
+      modificatorValue: "",
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
-      this.dialogVisible =false;
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
     },
     showDialog() {
       this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        );
+        this.posts = response.data;
+      } catch (e) {
+        alert("ошибка");
+      }
     },
   },
 };
